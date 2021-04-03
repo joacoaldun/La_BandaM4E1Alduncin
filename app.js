c0e3b4978ed14233a3698b8cfd8b4d1e750a10c0
joacoaldun <joacoaldun@gmail.com>
2021-04-03 13:32:50 -0300

    Primer commit-Joaquin Alducin-Register y login

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/registro', (req,res)=>{
+    res.sendFile(__dirname + '/views/registro.html');
+});
 
 app.listen(port, () => {
     console.log('Example app listening on port '+ port);
